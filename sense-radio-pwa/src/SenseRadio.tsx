1adf47990c79179c2173f91daa17ba8306e8b3b4
Peter Koller <peterkollerlv@live.com>
2022-02-05 13:32:30 +0100

    added molitio/ui-core radio page

--- sense-radio-pwa/src/SenseRadio.tsx
+++ sense-radio-pwa/src/SenseRadio.tsx
@@ -1,3 +1,19 @@
-import React from 'react';
+import React from "react";
+import {
+  RadioPage,
+  StyledThemeContextProvider,
+  ThemeNameTags,
+} from "@molitio/ui-core";
 
-export const SenseRadio: React.FC = () => { return <>video killed the radio star</> }
+export const SenseRadio: React.FC = () => {
+  return (
+    <StyledThemeContextProvider themeName={ThemeNameTags.BLACK_YELLOW}>
+      <RadioPage
+        radio={{
+          playUrl:
+            "https://s3.eu-west-1.amazonaws.com/filestore.molitio.org/nest-media/sense-radio/audio-store/chill-abstract-12099.mp3",
+        }}
+      ></RadioPage>
+    </StyledThemeContextProvider>
+  );
+};
