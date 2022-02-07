881b926dc47b02d3d65b341656c3ccac2a2edcfb
Peter Koller <peterkollerlv@live.com>
2022-02-07 08:50:27 +0100

    icons added

--- sense-radio-pwa/src/SenseRadio.tsx
+++ sense-radio-pwa/src/SenseRadio.tsx
@@ -8,12 +8,19 @@ import {
 export const SenseRadio: React.FC = () => {
   return (
     <StyledThemeContextProvider themeName={ThemeNameTags.BLACK_YELLOW}>
-      <RadioPage
-        radio={{
-          playUrl:
-            "https://s3.eu-west-1.amazonaws.com/filestore.molitio.org/nest-media/sense-radio/audio-store/chill-abstract-12099.mp3",
-        }}
-      ></RadioPage>
+      <div>
+        <RadioPage
+          radio={{
+            playUrl:
+              "https://s3.eu-west-1.amazonaws.com/filestore.molitio.org/nest-media/sense-radio/audio-store/chill-abstract-12099.mp3",
+          }}
+          logo={
+            <img
+              src={`https://s3.eu-west-1.amazonaws.com/filestore.molitio.org/nest-media/sense-radio/image-store/radio_sense_new_logo2.svg`}
+            ></img>
+          }
+        ></RadioPage>
+      </div>
     </StyledThemeContextProvider>
   );
 };
